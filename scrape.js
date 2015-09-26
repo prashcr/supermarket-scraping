ef85ede3d7f376a2d41787910b4083106cb7f2f4
Prashanth Chandra <coolshanth94@gmail.com>
2015-09-27 01:31:22 +0800

    Successfully insert but can't update MongoDB

--- scrape.js
+++ scrape.js
@@ -4,32 +4,72 @@ var async = require('async');
 var mongoose = require('mongoose');
 
 var baseUrl = 'http://www3.consumer.org.hk/pricewatch/supermarket/';
-var mongodbUrl = 'mongodb://localhost/groceries';
+var detailUrl = 'detail.php?itemcode=';
+var productSchema = new mongoose.Schema({
+  _id: String,
+  name: String,
+  brand: String,
+  category: { type: String, index: true },
+  prices: [{
+    date: { type: Date, default: Date.now },
+    wellcome: { price: String, discount: Boolean },
+    parknshop: { price: String, discount: Boolean },
+    marketplace: { price: String, discount: Boolean },
+    aeon: { price: String, discount: Boolean },
+    dch: { price: String, discount: Boolean }
+  }]
+});
+
+var Product = mongoose.model('Product', productSchema);
+mongoose.connect('mongodb://localhost/groceries');
+mongoose.connection.on('error', function() {
+  console.error('MongoDB Connection Error. Make Sure MongoDB is running.');
+});
 
 request(baseUrl + 'index.php?keyword=&lang=en', function(err, res, html) {
   if (!err && res.statusCode == 200) {
     var $ = cheerio.load(html);
-    // Get siblings of the header row of the table
+    // Get siblings of the table's header row
     var items = $('tr[bgcolor=#FED785]').siblings();
     items.each(function(i, el) {
       var cols = $(el).children();
-      // Get every column, except the first and last
       var category = $(cols).eq(1);
       var brand = $(cols).eq(2);
-      var product = $(cols).eq(3);
+      var name = $(cols).eq(3);
+      var itemcode = name.find('a').attr('href').substring(20);
       var wellcome = $(cols).eq(4);
       var parknshop = $(cols).eq(5);
       var marketplace = $(cols).eq(6);
       var aeon = $(cols).eq(7);
       var dch = $(cols).eq(8);
-      var url = product.find('a').attr('href');
+      var product = new Product({
+        _id: itemcode,
+        name: name.text(),
+        brand: brand.text(),
+        category: category.text()
+      });
+      Product.update(
+        product.toObject(),
+        {
+          $push: {
+            prices: {
+              wellcome: priceData(wellcome),
+              parknshop: priceData(parknshop),
+              marketplace: priceData(marketplace),
+              aeon: priceData(aeon),
+              dch: priceData(dch)
+            }
+          }
+        },
+        {upsert: true},
+        function(e) {if (e) console.log(e);});
     });
   }
 });
 
 // Normalize price data
 function priceData (price) {
-  if (price.text() == '--') return null;
+  if (price.text() == '--') return undefined;
   return {
     price: price.text().trim().replace(' ', '').substring(1),
     discount: Boolean(price.find('a').attr('href'))
