cfd8643e523975b5f3f012249962778945705436
Prashanth Chandra <coolshanth94@gmail.com>
2015-09-27 11:56:45 +0800

    Upserts working

--- scrape.js
+++ scrape.js
@@ -9,6 +9,7 @@ var productSchema = new mongoose.Schema({
   _id: String,
   name: String,
   brand: String,
+  // Queries will usually be on _id or category
   category: { type: String, index: true },
   prices: [{
     date: { type: Date, default: Date.now },
@@ -28,48 +29,63 @@ mongoose.connection.on('error', function() {
 
 request(baseUrl + 'index.php?keyword=&lang=en', function(err, res, html) {
   if (!err && res.statusCode == 200) {
-    var $ = cheerio.load(html);
+    var $ = cheerio.load(html); // Cheerio provides JQuery-like interface to DOM
+    console.log('HTML loaded');
     // Get siblings of the table's header row
     var items = $('tr[bgcolor=#FED785]').siblings();
+    console.log(items.length +' products parsed');
+    // For each item, extract data and upsert
     items.each(function(i, el) {
       var cols = $(el).children();
       var category = $(cols).eq(1);
       var brand = $(cols).eq(2);
       var name = $(cols).eq(3);
+      // Store id portion of product url
       var itemcode = name.find('a').attr('href').substring(20);
       var wellcome = $(cols).eq(4);
       var parknshop = $(cols).eq(5);
       var marketplace = $(cols).eq(6);
       var aeon = $(cols).eq(7);
       var dch = $(cols).eq(8);
-      var product = new Product({
-        _id: itemcode,
-        name: name.text(),
-        brand: brand.text(),
-        category: category.text()
-      });
+      // If query matches, update
+      // Otherwise, insert document created by combining query and update fields
+      // TODO: use async.parallel so we know when all updates have completed
       Product.update(
-        product.toObject(),
+        {
+          _id: itemcode,
+          name: name.text(),
+          brand: brand.text(),
+          category: category.text()
+        },
         {
           $push: {
-            prices: {
-              wellcome: priceData(wellcome),
-              parknshop: priceData(parknshop),
-              marketplace: priceData(marketplace),
-              aeon: priceData(aeon),
-              dch: priceData(dch)
-            }
+            prices: new Price(wellcome, parknshop, marketplace, aeon, dch)
           }
         },
         {upsert: true},
-        function(e) {if (e) console.log(e);});
+        function(e) {
+          if (e) return console.log(e);
+          console.log('upsert '+i+' successful');
+        });
     });
   }
 });
 
+// Constructor can handle case where all price fields are empty
+function Price(wellcome, parknshop, marketplace, aeon, dch) {
+  this.wellcome = priceData(wellcome);
+  this.parknshop = priceData(parknshop);
+  this.marketplace = priceData(marketplace);
+  this.aeon = priceData(aeon);
+  this.dch = priceData(dch);
+  if (!(this.wellcome || this.parknshop || this.marketplace || this.aeon || this.dch)) {
+    return undefined; // Mongoose ignores undefined fields
+  }
+}
+
 // Normalize price data
 function priceData (price) {
-  if (price.text() == '--') return undefined;
+  if (price.text() == '--') return undefined; // Mongoose ignores undefined fields
   return {
     price: price.text().trim().replace(' ', '').substring(1),
     discount: Boolean(price.find('a').attr('href'))
